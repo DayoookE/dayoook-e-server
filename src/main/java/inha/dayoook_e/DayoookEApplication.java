3275c0675d78ef3a1ffa475cf83a45e9a84c77f7
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-11-07 02:02:38 +0900

    setting: 프로젝트 기초 세팅

---
+++ src/main/java/inha/dayoook_e/DayoookEApplication.java
@@ -0,0 +1,13 @@
+package inha.dayoook_e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayoookEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayoookEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inha/dayoook_e/DayoookEApplicationTests.java
@@ -0,0 +1,13 @@
+package inha.dayoook_e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayoookEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
