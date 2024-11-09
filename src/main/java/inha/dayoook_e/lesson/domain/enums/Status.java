c34a47b3403aeac7cfdb176f9a474693a6901ba9
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-11-09 14:12:32 +0900

    feat/#3: 화상회의 엔티티 추가

---
+++ src/main/java/inha/dayoook_e/lesson/domain/enums/Status.java
@@ -0,0 +1,16 @@
+package inha.dayoook_e.lesson.domain.enums;
+
+public enum Status {
+
+    SCHEDULED("예정"), COMPLETED("완료"), CANCELED("취소");
+
+    private final String status;
+
+    Status(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
