5d64195b8c3b2f3008ea65c574c85d391f095065
gaau511 <gaau511@gmail.com>
2024-11-12 18:12:53 +0900

    feat/#11: Tutor Schedule Entity 변경

--- src/main/java/inha/dayoook_e/tutor/domain/repository/TutorScheduleJpaRepository.java
+++ src/main/java/inha/dayoook_e/tutor/domain/repository/TutorScheduleJpaRepository.java
@@ -1,8 +1,13 @@
 package inha.dayoook_e.tutor.domain.repository;
 
+import inha.dayoook_e.mapping.domain.Day;
+import inha.dayoook_e.mapping.domain.TimeSlot;
 import inha.dayoook_e.tutor.domain.TutorSchedule;
 import inha.dayoook_e.tutor.domain.id.TutorScheduleId;
+import inha.dayoook_e.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 /**
  * TutorScheduleJpaRepository는 TutorSchedule 엔티티에 대한 데이터 액세스 기능을 제공.
