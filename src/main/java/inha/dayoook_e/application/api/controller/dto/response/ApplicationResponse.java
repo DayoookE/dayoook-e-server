2e7d44da181440e2ff4e16ff04b1025b6cef55de
gaau511 <gaau511@gmail.com>
2024-11-13 16:05:15 +0900

    feat/#17: ApplicationResponse에서 신청 상태를 반환하는 필드 추가

--- src/main/java/inha/dayoook_e/application/api/controller/dto/response/ApplicationResponse.java
+++ src/main/java/inha/dayoook_e/application/api/controller/dto/response/ApplicationResponse.java
@@ -1,10 +1,18 @@
 package inha.dayoook_e.application.api.controller.dto.response;
 
+import inha.dayoook_e.application.domain.enums.Status;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
 
 public record ApplicationResponse (
     @NotNull
     @Schema(description = "신청 id", example = "1")
-    Integer id
+    Integer id,
+
+    @NotNull
+    @Schema(description = "신청 상태", example = "APPLYING")
+    @Enumerated(EnumType.STRING)
+    Status status
 ) {}
