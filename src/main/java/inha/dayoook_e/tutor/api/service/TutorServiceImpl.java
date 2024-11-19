6b8b023093238d03ec9e0b95ba2e9655c67d81d4
gaau511 <gaau511@gmail.com>
2024-11-19 15:46:28 +0900

    feat/#21: 튜터 목록 조회 기능 구현

--- src/main/java/inha/dayoook_e/tutor/api/service/TutorServiceImpl.java
+++ src/main/java/inha/dayoook_e/tutor/api/service/TutorServiceImpl.java
@@ -1,13 +1,40 @@
 package inha.dayoook_e.tutor.api.service;
 
 import inha.dayoook_e.application.domain.repository.ApplicationJpaRepository;
+import inha.dayoook_e.common.BaseEntity;
+import inha.dayoook_e.common.exceptions.BaseException;
+import inha.dayoook_e.mapping.api.controller.dto.response.SearchAgeGroupResponse;
+import inha.dayoook_e.mapping.api.controller.dto.response.SearchLanguagesResponse;
+import inha.dayoook_e.tutor.api.controller.dto.request.SearchCond;
+import inha.dayoook_e.tutor.api.controller.dto.response.SearchExperienceResponse;
+import inha.dayoook_e.tutor.api.controller.dto.response.TutorSearchPageResponse;
+import inha.dayoook_e.tutor.api.controller.dto.response.TutorSearchResponse;
 import inha.dayoook_e.tutor.api.mapper.TutorMapper;
+import inha.dayoook_e.tutor.domain.Experience;
+import inha.dayoook_e.tutor.domain.TutorAgeGroup;
+import inha.dayoook_e.tutor.domain.TutorInfo;
 import inha.dayoook_e.tutor.domain.repository.*;
+import inha.dayoook_e.user.api.mapper.UserMapper;
+import inha.dayoook_e.user.domain.User;
+import inha.dayoook_e.user.domain.UserLanguage;
 import inha.dayoook_e.user.domain.repository.UserJpaRepository;
+import inha.dayoook_e.user.domain.repository.UserLanguageJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static inha.dayoook_e.common.BaseEntity.State.ACTIVE;
+import static inha.dayoook_e.common.Constant.CREATE_AT;
+import static inha.dayoook_e.common.Constant.NAME;
+import static inha.dayoook_e.common.code.status.ErrorStatus.NOT_FIND_USER;
+import static inha.dayoook_e.common.code.status.ErrorStatus.TUTORINFO_NOT_FOUND;
 
 /**
  * TutorServiceImpl은 튜터 관련 비즈니스 로직을 처리하는 서비스 클래스.
@@ -19,12 +46,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class TutorServiceImpl implements TutorService {
 
 
-    private final TutorInfoJpaRepository tutorInfoJpaRepository;
     private final ExperienceJpaRepository experienceJpaRepository;
     private final TutorAgeGroupJpaRepository tutorAgeGroupJpaRepository;
-    private final ApplicationJpaRepository applicationJpaRepository;
-    private final TutorScheduleJpaRepository tutorScheduleJpaRepository;
+    private final UserLanguageJpaRepository userLanguageJpaRepository;
+    private final TutorQueryRepository tutorQueryRepository;
     private final UserJpaRepository userJpaRepository;
     private final TutorMapper tutorMapper;
+    private final UserMapper userMapper;
 
+    /**
+     * 튜터 목록 조건 조회
+     *
+     * @param searchCond 검색 조건
+     * @param page 페이지 번호
+     * @return 튜터 목록 조회 결과
+     */
+    @Override
+    public Slice<TutorSearchPageResponse> getTutors(SearchCond searchCond, Integer page) {
+        // 결과는 이름 순으로 정렬
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.ASC,NAME));
+
+        return tutorQueryRepository.searchTutors(searchCond, pageable);
+    }
 }
