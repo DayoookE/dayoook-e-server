301f8ca09e943101d7e58ae5422a38cbd9dea39d
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-12-13 18:43:55 +0900

    feat/#39: 튜터 이미지 추가

--- src/main/java/inha/dayoook_e/tutor/api/controller/dto/response/TutorSearchResponse.java
+++ src/main/java/inha/dayoook_e/tutor/api/controller/dto/response/TutorSearchResponse.java
@@ -36,6 +36,9 @@ public record TutorSearchResponse(
     @Schema(description = "한국어 수준", example = "BEGINNER")
     KoreanLevel koreanLevel,
 
+    @Schema(description = "프로필 이미지 URL", example = "https://dayoook-e.s3.ap-northeast-2.amazonaws.com/profile/1.jpg")
+    String profileUrl,
+
     @NotNull
     List<SearchAgeGroupResponse> ageGroups,
 
